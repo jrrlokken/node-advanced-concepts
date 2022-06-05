e0c11b735757a1170817c7ceeee1367013802656
Joshua Lokken <joshualokken@pm.me>
2022-06-05 12:04:18 -0500

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-  mongoURI:
-    "mongodb://nodeadvanced:ktvg802YzLI3u68d@node-blog-cluster-shard-00-00.nyjb9.mongodb.net:27017,node-blog-cluster-shard-00-01.nyjb9.mongodb.net:27017,node-blog-cluster-shard-00-02.nyjb9.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-9x6l5h-shard-0&authSource=admin&retryWrites=true&w=majority",
-  cookieKey: "123123123",
-};
